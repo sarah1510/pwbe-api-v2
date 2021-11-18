355f93410d0a99ab6d0ed5f18f43b623e262bff9
SARA APARECIDA SILVA DE FARIA <21192881@CFP127ALUNO.LOCAL>
2021-11-18 11:28:33 -0300

    .

--- controller/ControllerPessoa.php
+++ controller/ControllerPessoa.php
@@ -9,8 +9,20 @@ class ControllerPessoa{
     public function __construct($model){
 
         $this->_modelPessoa = $model;
-        $this->_method = 
+        $this->_method = $_SERVER['REQUEST_METHOD'];
 
+    }
+
+    function router(){
+        switch ($this->method) {
+            case 'value':
+                # code...
+                break;
+            
+            default:
+                # code...
+                break;
+        }
     }
     
 }
